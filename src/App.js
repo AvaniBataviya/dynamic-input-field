f432a5e1b6569526a8691c17178a7c6ea61f59f6
Avani Bataviya <avanibataviya2610@gmail.com>
2021-12-25 15:43:42 +0530

    Added dynamic input fields

--- src/App.js
+++ src/App.js
@@ -1,24 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import {
+  Button,
+  Card,
+  CardContent,
+  Container,
+  FormLabel,
+  Grid,
+  Stack,
+  TextField,
+  Typography
+} from '@mui/material';
+import { Box } from '@mui/system';
+
+const defaultData = {
+  firstName: '',
+  lastName: ''
+};
 
 function App() {
+  const [formInputs, setFormInputs] = useState([{ ...defaultData }]);
+
+  const onChange = (e, index) => {
+    const { name, value } = e.target;
+    const data = [...formInputs];
+    data[index][name] = value;
+    setFormInputs(data);
+  };
+
+  const onAddClick = () => {
+    setFormInputs([...formInputs, { ...defaultData }]);
+  };
+
+  const onRemoveClick = (index) => {
+    const data = [...formInputs];
+    data.splice(index, 1);
+    setFormInputs(data)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Stack alignItems='center'>
+      <Typography component="h2" variant='h4' mb={5}>
+        Add or Remove input fields dynamically with React Hooks
+      </Typography>
+      <Container maxWidth="md">
+        <Card variant='outlined'>
+          <CardContent>
+            <FormLabel component="legend">Enter name of colleague </FormLabel>
+            {
+              formInputs.map((element, index) => {
+                return (
+                  <Grid container spacing={2} key={index} alignItems='center'>
+                    <Grid container item xs={9}>
+                      <Grid item display='flex' width='100%'>
+                        <TextField
+                          label='First name'
+                          name='firstName'
+                          value={element.firstName}
+                          onChange={(e) => onChange(e, index)}
+                          margin='dense'
+                          fullWidth
+                          sx={{ mr: 1 }}
+                        />
+                        <TextField
+                          label='Last name'
+                          name='lastName'
+                          value={element.lastName}
+                          onChange={(e) => onChange(e, index)}
+                          margin='dense'
+                          fullWidth
+                        />
+                      </Grid>
+                    </Grid>
+                    <Grid item xs={3}>
+                      <Box display='flex' justifyContent='center' alignItems='center'>
+                        {
+                          formInputs.length - 1 === index &&
+                          <Button
+                            variant="contained"
+                            size='small'
+                            sx={{ ml: 2 }}
+                            onClick={onAddClick}>
+                            Add
+                          </Button>
+                        }
+                        {
+                          formInputs.length > 0 && formInputs.length !== 1 && (
+                            <Button
+                              variant="contained"
+                              size='small'
+                              color="error"
+                              sx={{ ml: 2 }}
+                              onClick={() => onRemoveClick(index)}>
+                              Remove
+                            </Button>
+                          )
+                        }
+                      </Box>
+                    </Grid>
+                  </Grid>
+                )
+              })
+            }
+            <Box mt={5}>
+              {JSON.stringify(formInputs)}
+            </Box>
+          </CardContent>
+        </Card>
+      </Container>
+    </Stack>
   );
 }
 
